474a70b011f4ea27fb51b6b73213f4b90fbd7365
SarahL94 <sarah.laoubi94@gmail.com>
2017-11-15 16:13:33 +0100

    Activity Memory

--- app/src/main/java/bintou/tpandroid/MainActivity.java
+++ app/src/main/java/bintou/tpandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package bintou.tpandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -54,6 +55,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -77,11 +79,11 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        Intent intent = new Intent(this, Memory.class);
+        startActivity(intent);
         int id = item.getItemId();
-
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
