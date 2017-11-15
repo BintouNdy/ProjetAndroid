c8e4561511d3da0395b8672ca3fb86a56129f53c
bintou <bintou.ndy@gmail.com>
2017-11-15 16:18:32 +0100

    jeu du morpion

--- app/src/main/java/bintou/tpandroid/MainActivity.java
+++ app/src/main/java/bintou/tpandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package bintou.tpandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,9 +82,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(this, Morpion.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
